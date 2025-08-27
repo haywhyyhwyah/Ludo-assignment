d9c56a911ec58c59b6fbc0d3244528284fe7cc62
haywhyyhwyah <ayomideisrael623@gmail.com>
2025-08-27 10:20:42 +0100

    Update

--- src/Components/Ludo.jsx
+++ src/Components/Ludo.jsx
@@ -4,13 +4,14 @@ import React, { useState } from "react";
 export default function App() {
     const [ludonumber, setludonum] = useState("");
 
+    const [ludsec, setludsec] = useState("")
 
     const number = () => {
         const number = Math.floor(1 + Math.random() * 6);
         setludonum(number);
 
         const secondNumber = Math.floor(1 + Math.random() * 6);
-        setludonum(secondNumber);
+        setludsec(secondNumber);
     };
 
     return (
@@ -28,7 +29,7 @@ export default function App() {
                         <p className="accountNumber">{ludonumber}</p>
 
                         <p className="label">Player 2</p>
-                        <p className="accountNumber">{ludonumber}</p>
+                        <p className="accountNumber">{ludsec}</p>
                     </div>
                 )}
 
