add59768b0efd33f24f94949065f6feffe5ee0e5
haywhyyhwyah <ayomideisrael623@gmail.com>
2025-08-27 10:08:03 +0100

    ludo assignment

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Ludo from './Components/Ludo'
+
+  const App = () => {
+  return (
+    <>
+      <Ludo/>
+    </>
+  );
+}
+
+export default App
---
+++ src/Components/Ludo.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+
+export default function App() {
+    const [ludonumber, setludonum] = useState("");
+
+
+    const number = () => {
+        const number = Math.floor(1 + Math.random() * 6);
+        setludonum(number);
+
+        const secondNumber = Math.floor(1 + Math.random() * 6);
+        setludonum(secondNumber);
+    };
+
+    return (
+        <div className="appContainer">
+            <div className="card">
+                <h1 className="title">Ludo Dice</h1>
+
+                <button onClick={number} className="btn">
+                    Shake Dice
+                </button>
+
+                {ludonumber && (
+                    <div className="result">
+                        <p className="label">Player 1</p>
+                        <p className="accountNumber">{ludonumber}</p>
+
+                        <p className="label">Player 2</p>
+                        <p className="accountNumber">{ludonumber}</p>
+                    </div>
+                )}
+
+            </div>
+        </div>
+    );
+}
